6e156bdcf540f28785c10900568317430fe79480
unou_sekki <unousekki@gmail.com>
2021-07-12 16:11:52 +0000

    did up to problem 4-3.

--- 1.FirstStep/test.hs
+++
@@ -1,4 +0,0 @@
-double x = x + x
-quadruple x = double(double x)
-factorial n = [1..5]
-avarage ns = sum ns `div` length ns
---
+++ practices/unit4.hs
@@ -0,0 +1,26 @@
+-- problem1
+halve :: [a] -> ([a], [a])
+halve xs = (take n xs, drop n xs)
+           where n = length xs `div` 2
+
+-- problem2
+third1 :: [a] -> a
+third1 xs = head(tail(tail xs))
+
+third2 :: [a] -> a
+third2 xs = xs !! 2
+
+third3 :: [a] -> a
+third3 (_:_:x:_) = x
+
+--problem3
+safetail1 :: [a] -> [a]
+safetail1 xs = if null xs then [] else tail xs
+
+safetail2 :: [a] -> [a]
+safetail2 xs | null xs = []
+             | otherwise = tail xs
+
+safetail3 :: [a] -> [a]
+safetail3 []     = []
+safetail3 (_:xs) = xs
