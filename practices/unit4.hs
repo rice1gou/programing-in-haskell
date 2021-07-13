414cbc8bf35672ece1df28faaea249dad3631aa9
rice1gou <rice1gou@gmail.com>
2021-07-13 15:49:59 +0000

    finished practices in Unit4

--- practices/unit4.hs
+++ practices/unit4.hs
@@ -26,3 +26,38 @@ safetail2 xs
 safetail3 :: [a] -> [a]
 safetail3 [] = []
 safetail3 (_ : xs) = xs
+
+--problem4
+
+(||) :: Bool -> Bool -> Bool
+True || True = True
+True || False = True
+False || True = True
+False || False = False
+
+--problem5
+
+(&&) :: Bool -> Bool -> Bool
+x && y =
+  if x
+    then if y then True else False
+    else False
+
+--problem7
+
+multi :: Int -> Int -> Int -> Int
+multi = \x -> (\y -> (\z -> x * y * z))
+
+--problem8
+
+luhnDouble :: Int -> Int
+luhnDouble n
+  | n > 9 = n -9
+  | otherwise = n
+
+luhn :: Int -> Int -> Int -> Int -> Bool
+luhn a b c d
+  | x `mod` 10 == 0 = True
+  | otherwise = False
+  where
+    x = sum [a, luhnDouble b, c, luhnDouble d]
